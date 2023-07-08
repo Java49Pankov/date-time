f558617b8da6efd71bf8ce1b37a009e283143646
Evgeny Pankov <volvovnl123@gmail.com>
2023-07-08 09:21:42 +0300

    HW #27 added method nextFriday13

--- src/telran/time/NextFriday13.java
+++ src/telran/time/NextFriday13.java
@@ -1,13 +1,29 @@
 package telran.time;
 
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.UnsupportedTemporalTypeException;
 
 public class NextFriday13 implements TemporalAdjuster {
 
 	@Override
 	public Temporal adjustInto(Temporal temporal) {
-		// TODO
-		return null;
+		if (!temporal.isSupported(ChronoUnit.MONTHS)) {
+			throw new UnsupportedTemporalTypeException("Temporal must support months");
+		}
+		temporal = adjust(temporal);
+		while (temporal.get(ChronoField.DAY_OF_WEEK) != 5) {
+			temporal = temporal.plus(1, ChronoUnit.MONTHS);
+		}
+		return temporal;
+	}
+
+	private Temporal adjust(Temporal temporal) {
+		if (temporal.get(ChronoField.DAY_OF_MONTH) >= 13) {
+			temporal = temporal.plus(1, ChronoUnit.MONTHS);
+		}
+		return temporal.with(ChronoField.DAY_OF_MONTH, 13);
 	}
 }
--- src/telran/time/test/DateTimeTests.java
+++ src/telran/time/test/DateTimeTests.java
@@ -29,7 +29,7 @@ class DateTimeTests {
 		TemporalAdjuster fr13 = new NextFriday13();
 		ZonedDateTime zdt = ZonedDateTime.now();
 		ZonedDateTime fr13Expected = ZonedDateTime.of(2023, 10, 13, 0, 0, 0, 0, ZoneId.systemDefault());
-		assertEquals(fr13Expected, zdt.with(fr13));
+		assertEquals(fr13Expected.toLocalDate(), zdt.with(fr13).toLocalDate());
 		LocalDate fr13Expected2 = LocalDate.of(2024, 9, 13);
 		LocalDate ld = LocalDate.of(2023, 10, 13);
 		assertEquals(fr13Expected2, ld.with(fr13));
